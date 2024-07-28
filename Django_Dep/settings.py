5aeb191b736be0a569b34e351416b5ef02db0c78
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-29 01:04:29 +0500

    Fix static files settings for Vercel deployment

--- Django_Dep/settings.py
+++ Django_Dep/settings.py
@@ -115,13 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+from pathlib import Path
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
