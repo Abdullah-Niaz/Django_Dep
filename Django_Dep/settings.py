f8aae16b0da1971ea7b025ba3373d79691cdd04b
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-29 00:42:25 +0500

    req,vercel files

--- Django_Dep/settings.py
+++ Django_Dep/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-j&c)*-%yxvfljr&j$b58ma2s#wv2gvp6ftk8s@hr#b6o^630me
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +117,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
